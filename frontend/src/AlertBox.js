678cca92ecaba77e8056ca870c0b198b01e0ca64
EloiseRosen <eloise.rosen@gmail.com>
2023-07-21 00:09:46 -0400

    initial files

---
+++ frontend/src/AlertBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function AlertBox() {
+  return (
+    <>
+      <p>Here is alert box.</p>
+    </>
+  );
+}
+
+export default AlertBox;
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AlertBox from './AlertBox';
+
+function App() {
+  return (
+    <>
+      <h1>AQI Me Baby</h1>
+      <AlertBox />
+    </>
+  );
+}
+
+export default App;
